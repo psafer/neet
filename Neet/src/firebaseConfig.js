7b6f41ec544b1ce079668044d0305778f120bb49
psafer <piotr.cierpial@outlook.com>
2024-09-19 19:13:48 +0200

    FirstDeployWorking

--- Neet/src/firebaseConfig.js
+++ Neet/src/firebaseConfig.js
@@ -1,9 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { getStorage } from 'firebase/storage';
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,7 +19,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
+const analytics = getAnalytics(app);
 
 // Initialize Firestore
 export const db = getFirestore(app);
