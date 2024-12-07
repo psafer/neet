849c89308abf067fe20848cf4b5d6566eb69b79d
psafer <piotr.cierpial@outlook.com>
2024-12-07 16:35:42 +0100

    ChatUpdate

--- Neet/src/components/chatComponents/ChatPanel.jsx
+++ Neet/src/components/chatComponents/ChatPanel.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { db, auth } from "../../firebaseConfig";
 import {
   collection,
   query,
@@ -9,6 +8,7 @@ import {
   doc,
   getDoc,
 } from "firebase/firestore";
+import { db, auth } from "../../firebaseConfig";
 import ChatHeader from "./ChatHeader";
 import MessageList from "./MessageList";
 import MessageInput from "./MessageInput";
@@ -97,32 +97,32 @@ const ChatPanel = () => {
   return (
     <div
       className={`fixed bottom-0 right-0 ${
-        isExpanded ? "w-full sm:w-96 h-[70vh]" : "w-16 h-16"
+        isExpanded ? "w-full sm:w-96 h-[70vh]" : "w-36 h-10"
       } bg-gray-800 shadow-lg transition-all duration-300 z-50`}
-      style={{
-        overflow: isExpanded ? "hidden" : "visible",
-      }}
     >
-      <ChatHeader onToggle={togglePanel} isExpanded={isExpanded} />
+      {/* Nagłówek - zawsze widoczny */}
+      <div className="h-12 bg-gray-900">
+        <ChatHeader onToggle={togglePanel} isExpanded={isExpanded} />
+      </div>
 
       {isExpanded && (
-        <div className="flex h-full">
+        <div className="flex h-[calc(100%-3rem)]">
           {/* Lista znajomych */}
-          <div className="w-1/3 bg-gray-700 p-2 overflow-y-auto">
+          <div className="w-1/3 bg-gray-700 p-2 overflow-y-auto border-r-2 border-slate-800">
             <h3 className="text-white font-bold mb-2">Znajomi</h3>
             {friends.length > 0 ? (
               friends.map((friend) => (
                 <div
                   key={friend.id}
                   onClick={() => openConversation(friend)}
-                  className="flex items-center p-2 bg-gray-600 text-white rounded cursor-pointer hover:bg-gray-500"
+                  className="flex items-center p-2 bg-gray-600 text-white cursor-pointer hover:bg-gray-500"
                 >
                   <img
                     src={friend.profilePicture}
                     alt={`${friend.name} profile`}
                     className="w-8 h-8 rounded-full mr-2"
                   />
-                  <p>{friend.name}</p>
+                  <p className="text-white text-sm">{friend.name}</p>
                 </div>
               ))
             ) : (
@@ -138,7 +138,7 @@ const ChatPanel = () => {
                 <MessageInput conversationId={activeConversationId} />
               </>
             ) : (
-              <div className="flex flex-col justify-center items-center flex-1 text-white">
+              <div className="flex flex-col ml-10 justify-center items-center flex-1 text-white">
                 <p>Wybierz znajomego, aby rozpocząć rozmowę</p>
               </div>
             )}
