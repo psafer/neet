c00c803d1b3472b60aeaff620063f45d80eb43b9
psafer <piotr.cierpial@outlook.com>
2024-12-15 11:32:15 +0100

    hotfixes

--- Neet/src/components/home/FilterPosts.jsx
+++ Neet/src/components/home/FilterPosts.jsx
@@ -1,13 +1,49 @@
 import PropTypes from "prop-types";
 import { HomeIcon, UserGroupIcon } from "@heroicons/react/24/solid";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db, auth } from "../../firebaseConfig";
 
-const FilterPosts = ({ currentFilter, setFilter }) => {
+const FilterPosts = ({ posts, setFilteredPosts }) => {
+  const [filter, setFilter] = useState("all");
+  const [followingList, setFollowingList] = useState([]);
+
+  // Pobierz listę znajomych (obserwowanych)
+  useEffect(() => {
+    const fetchFollowingList = async () => {
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const followersRef = collection(db, "followers");
+        const q = query(
+          followersRef,
+          where("followerId", "==", currentUser.uid)
+        );
+        const snapshot = await getDocs(q);
+        const following = snapshot.docs.map((doc) => doc.data().followingId);
+        setFollowingList(following);
+      }
+    };
+    fetchFollowingList();
+  }, []);
+
+  // Aktualizuj filtrowane posty na podstawie filtru
+  useEffect(() => {
+    if (filter === "all") {
+      setFilteredPosts(posts);
+    } else if (filter === "friends") {
+      const filtered = posts.filter((post) =>
+        followingList.includes(post.userId)
+      );
+      setFilteredPosts(filtered);
+    }
+  }, [filter, posts, followingList, setFilteredPosts]);
+
   return (
     <div className="w-full max-w-3xl flex justify-center mb-4">
       <button
         onClick={() => setFilter("all")}
         className={`px-4 py-2 w-1/2 flex justify-center items-center ${
-          currentFilter === "all"
+          filter === "all"
             ? "bg-orange-500 text-white"
             : "bg-gray-600 text-gray-200"
         } rounded-l-lg focus:outline-none`}
@@ -17,7 +53,7 @@ const FilterPosts = ({ currentFilter, setFilter }) => {
       <button
         onClick={() => setFilter("friends")}
         className={`px-4 py-2 w-1/2 flex justify-center items-center ${
-          currentFilter === "friends"
+          filter === "friends"
             ? "bg-orange-500 text-white"
             : "bg-gray-600 text-gray-200"
         } rounded-r-lg focus:outline-none`}
@@ -29,8 +65,8 @@ const FilterPosts = ({ currentFilter, setFilter }) => {
 };
 
 FilterPosts.propTypes = {
-  currentFilter: PropTypes.string.isRequired,
-  setFilter: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired,
+  setFilteredPosts: PropTypes.func.isRequired,
 };
 
 export default FilterPosts;
